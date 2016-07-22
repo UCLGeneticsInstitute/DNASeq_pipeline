73c03738d930acc60f3ad6638ac83f77ee809aac
pontikos <npontikos@gmail.com>
2016-07-22 13:21:40 +0100

    move cian files

--- UCLex/ciangene/scripts/Gene_Based_Tests/get_good_qual_snps.sh
+++
@@ -1,2 +0,0 @@
-R CMD BATCH get_good_qual_snps.R
-awk '{print $1}' SNP.data > SNP.data.extract
--- UCLex/ciangene/scripts/Gene_Based_Tests/merge_gene_tests.sh
+++
@@ -1,8 +0,0 @@
-ldak="/cluster/project8/vyp/cian/support/ldak/ldak"
-for dir in */
-do
-    echo $dir
-	$ldak --join-genes-reml $dir	
-done
-
-R CMD BATCH plot_results.R
