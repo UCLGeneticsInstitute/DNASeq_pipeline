5e5e7d1fbb3124fa77a9d306fe845210b1fd9807
Cian Murphy VYP <sejjcmu@bchuckle.local>
2016-09-17 22:23:57 +0100

    Improved SKAT function and added script to process reuslts after

--- ciangene/commit.sh
+++ ciangene/commit.sh
@@ -1,5 +1,5 @@
 git pull origin master
 git add -u
-git commit -m "Improved SKAT wrapper loads"
+git commit -m "Improved SKAT function and added script to process reuslts after"
 git push -u origin master
 
