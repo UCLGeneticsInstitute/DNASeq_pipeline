df3c56634473c3c7afd773500c80b831a08c71cb
pontikos <npontikos@gmail.com>
2017-01-10 15:16:02 +0000

    clean.R

--- annotation/filters/clean.R
+++ annotation/filters/clean.R
@@ -1,23 +1,30 @@
 #!/usr/bin/env Rscript
 err.cat <- function(x)  cat(x, '\n', file=stderr())
 
-### Series of filters suggested by Adam.
-message('*** CLEAN ***')
-
-# Filtering of variants based on annotation 
+suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(tools))
+suppressPackageStartupMessages(library(xtable))
 
-message('dim of input file')
-err.cat(dim( v <- read.csv(file('stdin')) ))
+d <- as.data.frame(fread('file:///dev/stdin',showProgress=FALSE))
+
+option_list <- list(
+    make_option(c('--out'), default='', help='out file')
+)
+
+option.parser <- OptionParser(option_list=option_list)
+opt <- parse_args(option.parser)
+
+message('*** CLEAN ***')
 
 #v <- v[,-which(colnames(v) %in% c('DISTANCE','CADD_RAW','CADD_PHRED','SOMATIC','CLIN_SIG','Feature_type'))]
+v <- v[,-which(colnames(v) %in% c('SYMBOL_SOURCE','HGNC_ID','CANONICAL','CADD_PHRED','GO','MISS','WT','AF'))]
 #v <- v[,-grep('ESP',colnames(v))]
-v <- v[,which(!grepl('STRAND|SYMBOL_SOURCE|HGNC_ID|CANONICAL',colnames(v)))]
-v <- v[,which(!grepl('geno',colnames(v)))]
-v <- v[,which(!grepl('depth',colnames(v)))]
+#v <- v[,which(!grepl('SYMBOL_SOURCE|HGNC_ID|CANONICAL|CADD_PHRED|GO|MISS|WT|AF',colnames(v)))]
+#v <- v[,which(!grepl('geno',colnames(v)))]
+#v <- v[,which(!grepl('depth',colnames(v)))]
 #
 #cleanup GO terms (is done in the GO filter)
 
-write.csv(v[order(v$R,decreasing=TRUE),], quote=FALSE, file='', row.names=FALSE)
 
+write.csv(d, file=opt$out, quote=FALSE, row.names=FALSE)
